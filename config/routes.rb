02d59a7b951eda3695451d5cf780c6d0c0eb5e40
William Sinn <w.h.sinn@gmail.com>
2019-04-30 10:48:11 -0400

    add owners routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'owners/index'
-  get 'owners/show'
-  get 'owners/new'
-  get 'owners/create'
-  get 'owners/edit'
+  get 'owners', to: 'owners#index'
+  get 'owners/new', to: 'owners#new'
+  post 'owners', to: 'owners#create'
+  get 'owners/:id', to: 'owners#show'
+  get 'owners/:id/edit', to: 'owners#edit'
   get 'owners/update'
   get 'owners/destroy'
 #  resources :owners, only: [:index, :show, :new, :create]
