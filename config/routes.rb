28b44f1f77d35b94e9c40ebccd3ede4ddc6201b8
William Sinn <w.h.sinn@gmail.com>
2019-04-30 10:38:42 -0400

    g controllers for pet and owners

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'owners/index'
+  get 'owners/show'
+  get 'owners/new'
+  get 'owners/create'
+  get 'owners/edit'
+  get 'owners/update'
+  get 'owners/destroy'
+#  resources :owners, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
