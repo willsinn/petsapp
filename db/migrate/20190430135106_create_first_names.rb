57b42a801756ddb223212e546280990a5021732e
William Sinn <w.h.sinn@gmail.com>
2019-04-30 09:55:53 -0400

    first commit

---
+++ db/migrate/20190430135106_create_first_names.rb
@@ -0,0 +1,11 @@
+class CreateFirstNames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :first_names do |t|
+      t.string :last_name
+      t.integer :age
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
